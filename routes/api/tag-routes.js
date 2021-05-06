373bdb7fcb3a3536096e0fcf708337bbfe33c928
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-06 15:16:09 -0400

    tag routes updated to include get all, fine one, update, and delete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -69,10 +69,40 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+      id:req.params.id
+      }
+    });
+    if (!updateTag) {
+      res.status(404).json({ message: "No new tag update!" });
+      return;
+    }
+    res.status(200).json(updateTag)
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!deleteTag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(deleteTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
