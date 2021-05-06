440664e64076b992b7dd8559a3ec13b0e4f5f655
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-06 15:29:58 -0400

    update category added to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,10 +43,33 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+      id:req.params.id
+      }
+    });
+    if (!updateCategory) {
+      res.status(404).json({ message: "No new category update!" });
+      return;
+    }
+    res.status(200).json(updateCategory)
+  } catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,28 +43,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
-  Tag.create({
-    tag_name: req.body.tag_name,
-  })
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  try {
+    const newTag = await Tag.create({
+      tag_name : req.body.tag_name
+   });
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
 router.put('/:id', (req, res) => {
