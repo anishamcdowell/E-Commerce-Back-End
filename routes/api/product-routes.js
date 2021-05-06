a76151ee9f2f495b4adb9c5c228db494873de3ff
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-06 14:51:03 -0400

    updated product routes to get all or one product and create a new product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,26 +6,63 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  // include its associated Category and Tag data
+  try {
+    const allProducts = await Product.findAll({
+      include: [
+        {
+          model: 'category',
+          attributes: ['id','category_name']
+        },
+        {
+          model: 'tag',
+          attributes: ['id','tag_name']
+        }
+      ] 
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const aProduct = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: 'category',
+          attributes: ['id','category_name']
+        },
+        {
+          model: 'tag',
+          attributes: ['id','tag_name']
+        }
+      ] 
+    });
+
+    if (!aProduct) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(aProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tag_id
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
