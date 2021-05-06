45724adb9bf26ed2731ea6a14d00cd896319827d
Anisha McDowell <anishamcdowell@gmail.com>
2021-05-06 15:08:06 -0400

    added delete to product router

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,15 +6,65 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+      include: [{
+          model: 'product',
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }] 
+    });
+    res.status(200).json(allTags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const aTag = await Tag.findByPk(req.params.id, {
+      include: [{
+        model: 'product',
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }] 
+    });
+
+    if (!aTag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(aTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
